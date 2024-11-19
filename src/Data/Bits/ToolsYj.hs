98d9881f00108d0bec4079d3332d08916b269739
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-11-19 09:38:15 +0900

    Data.Bits.ToolsYj: bitsList

--- src/Data/Bits/ToolsYj.hs
+++ src/Data/Bits/ToolsYj.hs
@@ -1,8 +1,12 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Data.Bits.ToolsYj (checkBits) where
+module Data.Bits.ToolsYj (checkBits, bitsList) where
 
 import Data.Bits
 
 checkBits :: Bits bs => bs -> bs -> Bool
 checkBits wnt = (== wnt) . (.&. wnt)
+
+bitsList :: FiniteBits bs => bs -> [bs]
+bitsList bs =
+	filter (/= zeroBits) $ map ((bs .&.) . bit) [0 .. finiteBitSize bs - 1]
