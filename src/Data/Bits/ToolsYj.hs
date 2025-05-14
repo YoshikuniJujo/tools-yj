14a9c6458bc74f49d8a0b89e001d3e277b8afd8f
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-05-14 15:56:59 +0900

    ByteString from/to Bits

--- src/Data/Bits/ToolsYj.hs
+++ src/Data/Bits/ToolsYj.hs
@@ -1,7 +1,8 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Data.Bits.ToolsYj (checkBits, bitsList) where
+module Data.Bits.ToolsYj (bitsToBits, checkBits, bitsList) where
 
+import Control.Arrow
 import Data.Bits
 
 checkBits :: Bits bs => bs -> bs -> Bool
@@ -10,3 +11,7 @@ checkBits wnt = (== wnt) . (.&. wnt)
 bitsList :: FiniteBits bs => bs -> [bs]
 bitsList bs =
 	filter (/= zeroBits) $ map ((bs .&.) . bit) [0 .. finiteBitSize bs - 1]
+
+bitsToBits :: (Bits a, Bits b) => Int -> a -> b
+bitsToBits n b = foldl setBit zeroBits
+	. map fst . filter snd $ map (id &&& testBit b) [0 .. n - 1]
---
+++ src/Data/ByteString/ToolsYj.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Data.ByteString.ToolsYj (
+
+	fromBits, fromBits', toBits, toBits'
+
+	) where
+
+import Data.Bits
+import Data.Bits.ToolsYj
+import Data.Bool
+import Data.ByteString qualified as BS
+
+fromBits :: Bits b => b -> BS.ByteString
+fromBits = BS.unfoldr \b ->
+	bool Nothing (Just (bitsToBits 8 b, b `shiftR` 8)) (b /= zeroBits)
+
+fromBits' :: FiniteBits b => b -> BS.ByteString
+fromBits' b0 = go (finiteBitSize b0 `div` 8) b0
+	where
+	go 0 _ = ""
+	go n b = bitsToBits 8 b `BS.cons` go (n - 1) (b `shiftR` 8)
+
+toBits :: Bits b => BS.ByteString -> b
+toBits = BS.foldr (\b s -> bitsToBits 8 b .|. s `shiftL` 8) zeroBits
+
+toBits' :: forall b . FiniteBits b => BS.ByteString -> Maybe b
+toBits' bs = bool
+	Nothing
+	(Just $ BS.foldr (\b s -> bitsToBits 8 b .|. s `shiftL` 8) zeroBits bs)
+	(8 * BS.length bs <= finiteBitSize @b undefined)
