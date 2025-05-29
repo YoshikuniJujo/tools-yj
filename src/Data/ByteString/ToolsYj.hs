9f3373ccd91c569c8a73255d5a39eb9ac3c0a3bd
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-05-29 15:59:17 +0900

    Data.ByteString.ToolsYj: fromBitsBE'

--- src/Data/ByteString/ToolsYj.hs
+++ src/Data/ByteString/ToolsYj.hs
@@ -7,7 +7,7 @@ module Data.ByteString.ToolsYj (
 
 	fromBits, fromBits', toBits, toBits',
 
-	toBitsBE
+	fromBitsBE', toBitsBE
 
 	) where
 
@@ -37,3 +37,9 @@ toBits' bs = bool
 
 toBitsBE :: Bits b => BS.ByteString -> b
 toBitsBE = BS.foldl (\s b -> bitsToBits 8 b .|. s `shiftL` 8) zeroBits
+
+fromBitsBE' :: FiniteBits b => b -> BS.ByteString
+fromBitsBE' b0 = go (finiteBitSize b0 `div` 8) b0
+	where
+	go 0 _ = ""
+	go n b = go (n - 1) (b `shiftR` 8) `BS.snoc` bitsToBits 8 b
