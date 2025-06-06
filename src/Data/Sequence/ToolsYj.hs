2b15bc2b95fdbe48f881a5e5db842413f2b26a2c
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-06 13:15:58 +0900

    Data.Sequence.ToolsYj: cons, snoc, uncons, unsnoc

--- src/Data/Sequence/ToolsYj.hs
+++ src/Data/Sequence/ToolsYj.hs
@@ -1,9 +1,28 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Data.Sequence.ToolsYj (splitAt') where
+module Data.Sequence.ToolsYj (
+
+	cons, snoc, uncons, unsnoc,
+
+	splitAt'
+
+	) where
 
 import Data.Sequence qualified as Seq
+
+cons :: a -> Seq.Seq a -> Seq.Seq a
+cons = (Seq.:<|)
+
+snoc :: Seq.Seq a -> a -> Seq.Seq a
+snoc = (Seq.:|>)
+
+uncons :: Seq.Seq a -> Maybe (a, Seq.Seq a)
+uncons = \case Seq.Empty -> Nothing; x Seq.:<| s -> Just (x, s)
+
+unsnoc :: Seq.Seq a -> Maybe (Seq.Seq a, a)
+unsnoc = \case Seq.Empty -> Nothing; s Seq.:|> x -> Just (s, x)
 
 splitAt' :: Int -> Seq.Seq a -> Maybe (Seq.Seq a, Seq.Seq a)
 splitAt' n s
