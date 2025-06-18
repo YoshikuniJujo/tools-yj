e75bd8fea2a248486fc76b458d217c2374f1efc8
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-18 14:37:17 +0900

    Control.Monad.ToolsYj: doWhile

--- src/Control/Monad/ToolsYj.hs
+++ src/Control/Monad/ToolsYj.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BlockArguments #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Control.Monad.ToolsYj where
@@ -16,3 +17,8 @@ whenMaybeDef = flip . maybe . pure
 
 doWhile_ :: Monad m => m Bool -> m ()
 doWhile_ a = fix $ (a >>=) . bool (pure ())
+
+doWhile :: Monad m => a -> (a -> m (Bool, a)) -> m a
+doWhile v0 a = ($ v0) $ fix \go v -> do
+	(c, v') <- a v
+	if c then go v' else pure v'
